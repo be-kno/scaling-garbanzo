917fdee2da0aceccce42e24b6b5192409d71bead
be-kno <156040342+be-kno@users.noreply.github.com>
2024-01-10 02:30:18 -0300

    upd: adding mongoose

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 const app = express();
+const port = process.env.PORT || 3000;
 
-let port = 3000;
+dotenv.config();
 
+app.use(express.json());
 app.set('view engine', 'ejs');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB :D '))
+  .catch(err => console.error('Could not connect to MongoDB :( '));
 
 app.get('/', (req, res) => {
     res.render("index")
@@ -11,4 +20,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
-})
+});
